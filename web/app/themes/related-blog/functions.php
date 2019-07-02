6325171d81fa581e5f138a8ea5fb743705a06a64
Allan Bendy <allan.bendy@gmail.com>
2019-07-02 15:45:35 -0400

    functions and vc default templates

--- web/app/themes/related-blog/functions.php
+++ web/app/themes/related-blog/functions.php
@@ -1,23 +1,12 @@
 <?php
 
-require_once 'includes/inc.theme-settings.php';
-require_once 'includes/inc.admin-ui.php';
-require_once 'includes/inc.visual-composer.php';
-require_once 'includes/inc.twig.php';
-require_once 'includes/inc.helpers.php';
-require_once 'includes/inc.breadcrumbs.php';
+require_once get_template_directory() . '/includes/inc.helpers.php';
+require_once get_template_directory() . '/includes/inc.admin-ui.php';
+require_once get_template_directory() . '/includes/inc.theme-settings.php';
+require_once get_template_directory() . '/includes/inc.acf.php';
+require_once get_template_directory() . '/includes/inc.visual-composer.php';
+require_once get_template_directory() . '/includes/inc.visual-composer.templates.php';
+require_once get_template_directory() . '/includes/inc.twig.php';
 
-function rltd_enqueue_styles() {
-    wp_enqueue_style( 'rltd-style',
-        get_template_directory_uri() . '/build/styles/main.css',
-        array(),
-        wp_get_theme()->get( 'Version' )
-    );
-}
-add_action( 'wp_enqueue_scripts', 'rltd_enqueue_styles' );
-
-function rltd_hide_parent( $themes ) {
-    unset( $themes[get_template()] );
-    return $themes;
-}
-add_filter( 'wp_prepare_themes_for_js','rltd_hide_parent' );
+// Load Custom Meta Box
+require_once get_template_directory() . '/includes/meta-box.php';
