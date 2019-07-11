474c07afb94625f4b6652a08220a8da9442e77ef
Allan Bendy <allan.bendy@gmail.com>
2019-07-11 11:23:56 -0400

    use index as main template

--- web/app/themes/related-blog/index.php
+++ web/app/themes/related-blog/index.php
@@ -1,1 +1,22 @@
 <?php
+
+use Timber\Timber;
+
+$context = Timber::get_context();
+
+$context['page'] = Timber::get_post();
+
+// Get image
+$context['page']->hero_image = wp_get_attachment_image_src( get_post_thumbnail_id( $context['page']->ID ), 'hero_full' )[0];
+
+// Get image alt tag
+$context['page']->hero_image_alt = get_post_meta( get_post_thumbnail_id( $context['page']->ID ), '_wp_attachment_image_alt', true );
+
+$context['password_required'] = post_password_required( $context['page']->ID );
+
+$context['menu'] = new \Timber\Menu( 'main' );
+$context['footer_menu'] = new \Timber\Menu( 'footer' );
+
+$context['is_front_page'] = is_front_page();
+
+Timber::render( 'index.twig', $context );
