10d83e7a3615232224cf7b3e4cb6362933b3d5a3
Allan Bendy <allan.bendy@gmail.com>
2019-07-11 12:17:41 -0400

    animate modules

--- web/app/themes/related-blog/functions.php
+++ web/app/themes/related-blog/functions.php
@@ -10,3 +10,8 @@ require_once get_template_directory() . '/includes/inc.twig.php';
 
 // Load Custom Meta Box
 require_once get_template_directory() . '/includes/meta-box.php';
+
+add_action( 'wp_enqueue_scripts', function() {
+  wp_register_style( 'animate-css', vc_asset_url( 'lib/bower/animate-css/animate.min.css' ), array(), WPB_VC_VERSION );
+  wp_enqueue_style( 'animate-css' );
+});
