68747b6f5c048042aae0bea43b40ff85e466962c
Allan Bendy <allan.bendy@gmail.com>
2019-07-15 10:26:04 -0400

    updates helper functions

--- web/app/themes/related-blog/includes/inc.helpers.php
+++ web/app/themes/related-blog/includes/inc.helpers.php
@@ -2,77 +2,54 @@
 
 // Get all custom content types
 function rltd_get_post_types( $output = 'objects', $friendly = null ) {
-    $args = array(
-        'public'   => true,
-        '_builtin' => true
-    );
-    $output   = $output;
-    $operator = 'and';
-    $post_types = get_post_types( $args, $output, $operator );
+  $args = array(
+    // 'public'   => true,
+    // '_builtin' => true
+  );
+  $output   = $output;
+  $operator = 'and';
+  $post_types = get_post_types( $args, $output, $operator );
 
-    if ( $friendly && is_array( $post_types ) && !empty( $post_types ) ) {
-        $postTypesList = array();
+  if ( $friendly && is_array( $post_types ) && !empty( $post_types ) ) {
+    $postTypesList = array();
 
-        foreach ( $post_types as $post_type ) {
-            $postTypesList[] = array(
-                $post_type->name,
-                $post_type->label,
-            );
-        }
-        asort( $postTypesList );
-
-        return $postTypesList;
+    foreach ( $post_types as $post_type ) {
+      $postTypesList[] = array(
+        $post_type->name,
+        $post_type->label,
+      );
     }
+    asort( $postTypesList );
 
-    return $post_types;
+    return $postTypesList;
+  }
+
+  return $post_types;
 }
 
 // Shared function for filtering queries by taxonomy
 function rltd_tax_query( $args, $taxonomies ) {
-    // Create array of all taxonony IDs
-    $taxonomies = explode( ',', $taxonomies );
+  // Create array of all taxonony IDs
+  $taxonomies = explode( ',', $taxonomies );
 
-    $taxonomies = array_map( 'trim', $taxonomies );
+  $taxonomies = array_map( 'trim', $taxonomies );
 
-    asort( $taxonomies );
+  asort( $taxonomies );
 
-    $args['tax_query'] = array( 'relation' => 'OR' );
+  $args['tax_query'] = array( 'relation' => 'OR' );
 
-    // Loop over taxonomy and build query filter array
-    foreach ( $taxonomies as $taxonomy ) {
-        $term = get_term( $taxonomy );
+  // Loop over taxonomy and build query filter array
+  foreach ( $taxonomies as $taxonomy ) {
+    $term = get_term( $taxonomy );
 
-        $tax_query = array( array(
-            'taxonomy' => $term->taxonomy,
-            'field' => 'slug',
-            'terms' => $term->slug,
-        ) );
+    $tax_query = array( array(
+      'taxonomy' => $term->taxonomy,
+      'field' => 'slug',
+      'terms' => $term->slug,
+    ) );
 
-        $args['tax_query'][] = $tax_query;
-    }
+    $args['tax_query'][] = $tax_query;
+  }
 
-    return $args;
-}
-
-// Grouping function
-function rltd_groupby_tax_clauses( $clauses, $wp_query ) {
-    global $wpdb;
-
-    if ( isset( $wp_query->query['orderby'] ) ) {
-        // Include a count field
-        $clauses['fields'] = 'wp_posts.ID, COUNT( t.term_id ) AS count';
-
-        $clauses['join'] .=<<<SQL
- LEFT OUTER JOIN {$wpdb->term_relationships} tr ON {$wpdb->posts}.ID = tr.object_id
- LEFT OUTER JOIN {$wpdb->term_taxonomy} tt ON tr.term_taxonomy_id = tt.term_taxonomy_id
- LEFT OUTER JOIN {$wpdb->terms} t USING ( term_id )
-SQL;
-
-        $clauses['where'] .= " AND ( taxonomy = 'post_tag' )";
-        $clauses['groupby'] = "t.term_id";
-        $clauses['orderby']  = "GROUP_CONCAT( t.name ORDER BY name ASC ) ";
-        $clauses['orderby'] .= ( 'ASC' == strtoupper( $wp_query->get( 'order' ) ) ) ? 'ASC' : 'DESC';
-    }
-
-    return $clauses;
+  return $args;
 }
