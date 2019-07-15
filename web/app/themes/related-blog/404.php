50385d1dd96cffddce5b874c1feff4dc017d038b
Allan Bendy <allan.bendy@gmail.com>
2019-07-15 12:40:34 -0400

    404 page

--- web/app/themes/related-blog/404.php
+++ web/app/themes/related-blog/404.php
@@ -1,4 +1,10 @@
 <?php
 
+use Timber\Timber;
+
 $context = Timber::get_context();
+
+$context['menu'] = new \Timber\Menu( 'main' );
+$context['footer_menu'] = new \Timber\Menu( 'footer' );
+
 Timber::render( '404.twig', $context );
