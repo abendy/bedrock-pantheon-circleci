35e92062b707ce8742b9e71738b131e63d378371
Allan Bendy <allan.bendy@gmail.com>
2019-07-15 10:35:09 -0400

    updates functions php

--- web/app/themes/related-blog/functions.php
+++ web/app/themes/related-blog/functions.php
@@ -3,15 +3,14 @@
 require_once get_template_directory() . '/includes/inc.helpers.php';
 require_once get_template_directory() . '/includes/inc.admin-ui.php';
 require_once get_template_directory() . '/includes/inc.theme-settings.php';
-require_once get_template_directory() . '/includes/inc.acf.php';
+
+require_once get_template_directory() . '/includes/inc.custom-post-types.php';
+require_once get_template_directory() . '/includes/custom-post.php';
+
+// require_once get_template_directory() . '/includes/inc.acf.php';
 require_once get_template_directory() . '/includes/inc.visual-composer.php';
 require_once get_template_directory() . '/includes/inc.visual-composer.templates.php';
 require_once get_template_directory() . '/includes/inc.twig.php';
 
-// Load Custom Meta Box
 require_once get_template_directory() . '/includes/meta-box.php';
 
-add_action( 'wp_enqueue_scripts', function() {
-  wp_register_style( 'animate-css', vc_asset_url( 'lib/bower/animate-css/animate.min.css' ), array(), WPB_VC_VERSION );
-  wp_enqueue_style( 'animate-css' );
-});
