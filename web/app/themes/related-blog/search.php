1afc61c5150d06062861660275aef184c2330c7a
Allan Bendy <allan.bendy@gmail.com>
2019-07-15 17:03:21 -0400

    adds search module and styles

--- web/app/themes/related-blog/search.php
+++ web/app/themes/related-blog/search.php
@@ -1,35 +1,14 @@
 <?php
 
-get_header();
-
-// Set the location for the Twig tpls.
-if ( class_exists( 'Timber' ) ) {
-  Timber::$dirname = array( '../views/modules', '../views/page' );
-}
-
 use Timber\Timber;
 
-// Twigify
-$context = Timber::get_context();
-$posts = Timber::get_posts();
+$context          = Timber::get_context();
 
-$add_breadcrumbs = new Rltd_Breadcrumbs();
-$context['posts'] = $add_breadcrumbs->set_parent( $posts );
+$context['title'] = 'Search results for ' . get_search_query();
 
-global $wp_query;
-$context['found_posts'] = $wp_query->found_posts;
-$context['search_query'] = get_search_query();
-$context['pagination'] = get_the_posts_pagination( array(
-                            'mid_size'  => 2,
-                            'prev_text' => __( 'Previous', 'related-blog' ),
-                            'next_text' => __( 'Next ', 'related-blog' ),
-                        ) );
+$context['posts'] = new \Timber\PostQuery();
 
-Timber::render(
-    array( 'views/page/page.search.twig' ),
-    $context
-);
+$context['menu'] = new \Timber\Menu( 'main' );
+$context['footer_menu'] = new \Timber\Menu( 'footer' );
 
-get_sidebar();
-
-get_footer();
+Timber::render( 'search.twig', $context );
