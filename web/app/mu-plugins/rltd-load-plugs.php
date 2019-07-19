3125c027bb62a571cf3c762363bf1efc47e705c6
Allan Bendy <allan.bendy@gmail.com>
2019-07-19 10:54:46 -0400

    activate essential plugs

--- web/app/mu-plugins/rltd-load-plugs.php
+++ web/app/mu-plugins/rltd-load-plugs.php
@@ -10,16 +10,6 @@ if (!is_blog_installed()) {
   return;
 }
 
-// require_once WPMU_PLUGIN_DIR . '/advanced-custom-fields/acf.php';
-
-// if (!class_exists('Vc_Manager')) {
-//     require_once WPMU_PLUGIN_DIR . '/js_composer/js_composer.php';
-// }
-
 if (!class_exists('Timber')) {
   require_once WP_PLUGIN_DIR . '/timber-library/timber.php';
 }
-
-// if (!class_exists('Options_Framework') and !function_exists('optionsframework_init')) {
-//     require_once WPMU_PLUGIN_DIR . '/options-framework/options-framework.php';
-// }
