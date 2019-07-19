081e5d96e06ac9bb29c93469c9efe38370b146db
Allan Bendy <allan.bendy@gmail.com>
2019-07-19 14:23:12 -0400

    register acf fields

--- web/app/themes/related-blog/functions.php
+++ web/app/themes/related-blog/functions.php
@@ -7,7 +7,7 @@ require_once get_template_directory() . '/includes/inc.theme-settings.php';
 require_once get_template_directory() . '/includes/inc.custom-post-types.php';
 require_once get_template_directory() . '/includes/inc.custom-post-helpers.php';
 
-// require_once get_template_directory() . '/includes/inc.acf.php';
+require_once get_template_directory() . '/includes/inc.acf.php';
 require_once get_template_directory() . '/includes/inc.visual-composer.php';
 require_once get_template_directory() . '/includes/inc.visual-composer.templates.php';
 require_once get_template_directory() . '/includes/inc.twig.php';
