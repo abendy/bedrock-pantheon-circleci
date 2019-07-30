d6d90618f95014ccea7e2c3f67ca0e13888cafd9
Allan Bendy <allan.bendy@gmail.com>
2019-07-30 15:17:48 -0400

    fixes duplicate page title display

--- web/app/themes/related-blog/single.php
+++ web/app/themes/related-blog/single.php
@@ -8,6 +8,34 @@ $context['page'] = Timber::get_post();
 $context['password_required'] = post_password_required( $context['page']->ID );
 $context['is_front_page'] = is_front_page();
 
+// If the hero is using the hero title as the page title
+// remove the regular WP page title
+
+$pattern = get_shortcode_regex( array( 'rltd_hero' ) );
+
+$content = preg_match( "/$pattern/", $context['page']->post_content, $matches );
+
+if ( $matches ) {
+  $atts = shortcode_parse_atts( $matches[3] );
+
+  extract(
+    shortcode_atts(
+      array(
+        'rltd_hero_container' => '',
+      ),
+      $atts
+    )
+  );
+
+  $container = !empty( $rltd_hero_container ) ? vc_param_group_parse_atts( $rltd_hero_container ) : array();
+
+  if ( !empty( reset( $container )['rltd_hero_item_use_as_header'] ) && reset( $container )['rltd_hero_item_use_as_header'] === 'yes' ) {
+    $context['page']->post_title = '';
+  }
+}
+
+
+
 $has_hero = preg_match( '/\[rltd_hero/', $context['page']->post_content );
 
 if ( !$has_hero ) {
