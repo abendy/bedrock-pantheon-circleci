f70a7c7ea807184d6e22aad18c78414dbf0db729
Allan Bendy <allan.bendy@gmail.com>
2019-07-30 20:15:31 -0400

    search results 9 posts per page

--- web/app/themes/related-blog/search.php
+++ web/app/themes/related-blog/search.php
@@ -6,6 +6,10 @@ $context          = Timber::get_context();
 
 $context['title'] = 'Search results for ' . get_search_query();
 
-$context['posts'] = new \Timber\PostQuery();
+$args = array(
+  'posts_per_page' => 9,
+);
+
+$context['posts'] = new \Timber\PostQuery( $args );
 
 Timber::render( 'search.twig', $context );
