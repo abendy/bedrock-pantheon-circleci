e9c3cb2b9846fb5d5f794f280534d943ff6b8fd0
Allan Bendy <allan.bendy@gmail.com>
2019-08-01 12:02:11 -0400

    updates pagination for search

--- web/app/themes/related-blog/search.php
+++ web/app/themes/related-blog/search.php
@@ -10,6 +10,15 @@ $args = array(
   'posts_per_page' => 9,
 );
 
+// Filter by pagination
+global $paged;
+
+if ( !isset( $paged ) || !$paged ) {
+  $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : ( ( get_query_var( 'page' ) ) ? get_query_var( 'page' ) : 1 );
+}
+
+$args['paged'] = $paged;
+
 $context['posts'] = new \Timber\PostQuery( $args );
 
 Timber::render( 'search.twig', $context );
