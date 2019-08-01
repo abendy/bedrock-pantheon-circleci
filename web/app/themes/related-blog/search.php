f6827c0ccc14129abe920f54000ca43931d7e879
Allan Bendy <allan.bendy@gmail.com>
2019-08-01 12:27:36 -0400

    fixes search

--- web/app/themes/related-blog/search.php
+++ web/app/themes/related-blog/search.php
@@ -6,11 +6,24 @@ $context          = Timber::get_context();
 
 $context['title'] = 'Search results for ' . get_search_query();
 
+// Set search arguments
+
 $args = array(
   'posts_per_page' => 9,
 );
 
+// Filter by search query
+
+global $s;
+
+if ( !isset( $s ) || !$s ) {
+  $s = @get_query_var( 's' );
+}
+
+$args['s'] = $s;
+
 // Filter by pagination
+
 global $paged;
 
 if ( !isset( $paged ) || !$paged ) {
@@ -19,6 +32,10 @@ if ( !isset( $paged ) || !$paged ) {
 
 $args['paged'] = $paged;
 
+// Run search query
+
 $context['posts'] = new \Timber\PostQuery( $args );
+
+// Render
 
 Timber::render( 'search.twig', $context );
