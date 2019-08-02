c16bdef865c429c9bbe95ca6408bdbfe88b46605
Allan Bendy <allan.bendy@gmail.com>
2019-08-02 13:33:01 -0400

    adds lazyload for content list images

---
+++ web/app/themes/related-blog/src/scripts/lazysizes.js
@@ -0,0 +1,8 @@
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+
+window.lazySizesConfig = window.lazySizesConfig || {};
+window.lazySizesConfig.customMedia = {
+  '--medium': '(max-width: 768px)',
+  '--large': '(max-width: 1024px)',
+};
