bf1da939fa301ba7d97faf7f782630eefbc290f5
Allan Bendy <allan.bendy@gmail.com>
2019-08-02 13:32:40 -0400

    allow custom css on vc column elems

--- web/app/themes/related-blog/templates/vc/vc_column.php
+++ web/app/themes/related-blog/templates/vc/vc_column.php
@@ -4,7 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( '-1' );
 }
 
-$output = $width = $offset = '';
+$width = $offset = $el_class = $output = '';
 
 $atts = vc_map_get_attributes( $this->getShortcode(), $atts );
 extract( $atts );
@@ -12,15 +12,18 @@ extract( $atts );
 $width = rltd_wpb_translateColumnWidthToSpan( $width );
 $width = vc_column_offset_class_merge( $offset, $width );
 
+$custom_class = esc_attr( trim( vc_shortcode_custom_css_class( $css ) ) );
+
 $css_classes = array(
   'main-col',
   'column',
   $width,
+  $custom_class,
 );
 
 $wrapper_attributes = array();
 
-$css_class = preg_replace( '/\s+/', ' ', implode( ' ', array_filter( $css_classes ) ) );
+$css_class = preg_replace( '/\s+/', ' ', apply_filters( VC_SHORTCODE_CUSTOM_CSS_FILTER_TAG, implode( ' ', array_filter( $css_classes ) ), $this->settings['base'], $atts ) );
 $wrapper_attributes[] = 'class="' . esc_attr( trim( $css_class ) ) . '"';
 
 $output .= '<div ' . implode( ' ', $wrapper_attributes ) . '>';
